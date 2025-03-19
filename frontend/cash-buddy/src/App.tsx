95046cfbc8aaaeec35399e84357846f17b6f46e7
bm <bm.devandlearn@gmail.com>
2025-03-19 19:22:40 +0100

    forms from chat gpt

--- frontend/cash-buddy/src/App.tsx
+++ frontend/cash-buddy/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import {Frequency} from "./types/Frequency.ts";
-import CategoryItem from "./components/CategoryItem.tsx";
-import {expenses} from "./data/mockData.ts";
+
+
+import AddExpenseForm from "./components/AddExpenseForm.tsx";
 
 const App: React.FC = () => (
     <div className="app">
         <h1>Cash Buddy</h1>
-        <CategoryItem description={"Test"} limit={11.22} type={Frequency.ANNUAL} expenses={expenses}/>
+        <AddExpenseForm />
+        {/*<CategoryItem description={"Test"} limit={11.22} type={Frequency.ANNUAL} expenses={expenses}/>*/}
         {/*<CategoryList categories={categories} initialExpenses={initialExpenses} />*/}
     </div>
 );
